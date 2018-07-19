692134000c471317891ba96e1bde10b788467709
MortyraSky <akonan-doil@yandex.ru>
2018-07-19 10:11:04 +0300

    pageobj

--- src/test/java/com/gmail/yurok058/tests/FirstTest.java
+++ src/test/java/com/gmail/yurok058/tests/FirstTest.java
@@ -1,5 +1,6 @@
-package com.gmail.yurok058;
+package com.gmail.yurok058.tests;
 
+import com.gmail.yurok058.pages.PoiskPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -12,32 +13,40 @@ import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
-public class FirstTest {
+public class FirstTest extends ManageDriver{
 
     private static WebDriver driver;
 
-    @BeforeClass
+    /*@BeforeClass
     public static void start() {
-        System.setProperty("webdriver.chrome.driver", "c:/Users/MacPro/Work/chromedriver.exe");
+        ///System.setProperty("webdriver.chrome.driver", "c:/Users/MacPro/Work/chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
         driver.get("https://ya.ru/");
+    }*/
+    @Test
+    public void SearchWeather(){
+        poiskPage.inputINsearch("Погода пенза");
+        poiskPage.clickSearchButt();
+
     }
     @Test
-    public void weatherintown() {
-        WebElement inputfield = driver.findElement(By.cssSelector("input[tabindex='2']"));
+    public void Weatherintown() {
+        /*WebElement inputfield = driver.findElement(By.cssSelector("input[tabindex='2']"));
         inputfield.sendKeys("погода пенза");
         WebElement searchbutt = driver.findElement(By.cssSelector("button[type='submit']"));
         searchbutt.click();
-        WebElement prognozlink = driver.findElement(By.cssSelector("a[accesskey*='1']"));
-        String txtprognozlink = prognozlink.getText();
+        WebElement prognozlink = driver.findElement(By.cssSelector("a[accesskey*='1']"));*/
+
+        String txtprognozlink = poiskResult.getTxtPrognozLink();
         Assert.assertTrue(txtprognozlink.contains("Погода"));
     }
-    @AfterClass
+
+   /* @AfterClass
     public static void finish() {
-        WebElement onmain = driver.findElement(By.cssSelector("a[class*='ys-ru']"));
-        onmain.click();
+        //WebElement onmain = driver.findElement(By.cssSelector("a[class*='ys-ru']"));
+        //onmain.click();
         driver.quit();
-    }
+    }*/
 }
